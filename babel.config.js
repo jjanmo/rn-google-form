cafaa335e6d86e2959b4e560aa52e15536b094a0
jjanmo <jjanmo07@hanmail.net>
2023-08-15 15:36:21 +0900

    feat: configure absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          root: ['.'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@hooks': './src/hooks',
+            '@store': './src/store',
+            '@styles': './src/styles',
+          },
+        },
+      ],
+    ],
+  }
+}
