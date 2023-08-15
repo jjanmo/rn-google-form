ef594dd54f5b16e4f0ebac3a6917f0f0ab82133b
jjanmo <jjanmo07@hanmail.net>
2023-08-16 07:12:33 +0900

    feat: update type-selector ui

--- src/components/CardTypeSelector.tsx
+++ src/components/CardTypeSelector.tsx
@@ -1,10 +1,11 @@
 import { useActionSheet } from '@expo/react-native-action-sheet'
-import { Button } from '@react-native-material/core'
+import { Button, Text } from '@react-native-material/core'
 import { useDispatch } from 'react-redux'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { colors } from '@styles/theme'
 import { cardActions } from '@store/slice/cardSlice'
 import { SurveyCardTypeKey } from '@store/slice/cardSlice.type'
+import { useState } from 'react'
 
 interface Props {
   id: string
@@ -13,8 +14,9 @@ interface Props {
 export default function CardTypeSelector({ id }: Props) {
   const dispatch = useDispatch()
   const { showActionSheetWithOptions } = useActionSheet()
+  const [type, setType] = useState('객관식 질문')
 
-  const onPress = () => {
+  const handlePress = () => {
     const options = ['단답형', '장문형', '객관식 질문', '체크박스', '취소']
 
     showActionSheetWithOptions(
@@ -28,12 +30,22 @@ export default function CardTypeSelector({ id }: Props) {
         const typeKey = options[i as number]
         const type = typeMap[typeKey]
         dispatch(cardActions.updateCardType({ id, type }))
+        setType(typeKey)
       }
     )
   }
 
   return (
-    <Button title="설문지 타입 설정" variant="outlined" onPress={onPress} color={colors.greyDark} />
+    <View style={styles.container}>
+      <Button
+        title="설문지 타입 설정"
+        variant="outlined"
+        onPress={handlePress}
+        color={colors.greyDark}
+        style={styles.button}
+      />
+      <Text style={styles.text}>{type}</Text>
+    </View>
   )
 }
 
@@ -43,3 +55,22 @@ const typeMap: Record<string, SurveyCardTypeKey> = {
   '객관식 질문': 'radio',
   체크박스: 'checkbox',
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    width: '50%',
+  },
+  text: {
+    width: '45%',
+    paddingVertical: 8,
+    fontSize: 18,
+    textAlign: 'center',
+    color: colors.greyDark,
+    backgroundColor: colors.greyLight,
+  },
+})
