02f1241cd7e78288c07c7f5f8f05b9057b340b9d
jjanmo <jjanmo07@hanmail.net>
2023-08-16 01:59:01 +0900

    fix: make a home render-change in accord with state structure

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,27 +2,34 @@ import Layout from '@components/Layout'
 import { CheckboxCard, LongCard, RadioCard, ShortCard, TitleCard } from '@components/Card'
 import { shallowEqual, useSelector } from 'react-redux'
 import { RootState } from '@store/root'
-import { CardType } from '@store/slice/cardSlice'
+import { CardType } from '@store/slice/cardSlice.type'
+import { ActionSheetProvider } from '@expo/react-native-action-sheet'
+
+export default function Home() {
+  const cards = useSelector<RootState, Record<string, CardType>>(
+    (state) => state.cards.data,
+    shallowEqual
+  )
+  // const all = useSelector<RootState,any>((state) => state.cards, shallowEqual)
+  console.log(cards, cards.length)
+  return (
+    <ActionSheetProvider>
+      <Layout>
+        {Object.keys(cards).map((key) => {
+          const card = cards[key]
+          if (card.type === 'title') return <TitleCard key={key} {...card} />
+
+          const CardComponent = cardComponentMap[card.type]
+          return <CardComponent key={key} {...card} />
+        })}
+      </Layout>
+    </ActionSheetProvider>
+  )
+}
 
 const cardComponentMap = {
   short: ShortCard,
   long: LongCard,
   radio: RadioCard,
   checkbox: CheckboxCard,
-}
-
-export default function Home() {
-  const cards = useSelector<RootState, CardType[]>((state) => state.cards.data, shallowEqual)
-  console.log(cards, cards.length)
-  return (
-    <Layout>
-      {cards.map((card) => {
-        const { id, type } = card
-        if (type === 'title') return <TitleCard key={id} {...card} />
-
-        const CardComponent = cardComponentMap[type]
-        return <CardComponent key={id} {...card} />
-      })}
-    </Layout>
-  )
 }
