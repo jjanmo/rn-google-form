48eea5e776cb0a777201f61eed0197efb68549d5
jjanmo <jjanmo07@hanmail.net>
2023-08-16 20:56:04 +0900

    fix: fix layout and home screen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,39 +1,11 @@
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleProp,
-  StyleSheet,
-  TextStyle,
-  View,
-  ViewStyle,
-} from 'react-native'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { PropsWithChildren } from 'react'
-import { useDispatch } from 'react-redux'
-import { MaterialIcons } from '@expo/vector-icons'
-import { cardActions } from '@store/slice/cardSlice'
 import { colors } from '@styles/theme'
-import Button, { ButtonCustomStyles } from './Button'
 
 export default function Layout({ children }: PropsWithChildren) {
-  const dispatch = useDispatch()
-
-  const handlePressAdd = () => {
-    dispatch(cardActions.addCard())
-  }
-
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <Button
-          buttonText="항목 추가"
-          onPress={handlePressAdd}
-          renderIcon={() => (
-            <MaterialIcons name="add-circle-outline" size={20} color={colors.greyDark} />
-          )}
-          styles={buttonCustomStyles}
-        />
-        <ScrollView contentContainerStyle={styles.scrollview}>{children}</ScrollView>
-      </View>
+      <View style={styles.container}>{children}</View>
     </SafeAreaView>
   )
 }
@@ -47,23 +19,4 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: colors.purpleLight,
   },
-  scrollview: {
-    paddingBottom: 30,
-    marginVertical: 20,
-    alignItems: 'center',
-  },
 })
-
-const buttonCustomStyles: ButtonCustomStyles = {
-  button: {
-    paddingVertical: 10,
-    marginHorizontal: 10,
-    justifyContent: 'center',
-    backgroundColor: colors.purple,
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: colors.greyDark,
-  },
-}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,14 @@
-import { shallowEqual, useSelector } from 'react-redux'
+import { ScrollView, StyleSheet } from 'react-native'
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import { ActionSheetProvider } from '@expo/react-native-action-sheet'
+import { MaterialIcons } from '@expo/vector-icons'
 import { RootState } from '@store/root'
 import { CardType } from '@store/slice/cardSlice.type'
+import { cardActions } from '@store/slice/cardSlice'
 import { SelectCard, TextCard, TitleCard } from '@components/Card'
 import Layout from '@components/Layout'
+import Button, { ButtonCustomStyles } from '@components/Button'
+import { colors } from '@styles/theme'
 
 export default function Home() {
   const cards = useSelector<RootState, CardType[]>((state) => state.cards.data, shallowEqual)
@@ -12,15 +17,31 @@ export default function Home() {
   const activeCard = useSelector<RootState, any>((state) => state.cards.activeCard, shallowEqual)
   console.log(cards, cards.length, activeCard)
 
+  const dispatch = useDispatch()
+
+  const handlePressAdd = () => {
+    dispatch(cardActions.addCard())
+  }
+
   return (
     <ActionSheetProvider>
       <Layout>
-        {cards.map((card) => {
-          if (card.type === 'title') return <TitleCard key={card.id} {...card} />
+        <Button
+          buttonText="항목 추가"
+          onPress={handlePressAdd}
+          renderIcon={() => (
+            <MaterialIcons name="add-circle-outline" size={20} color={colors.greyDark} />
+          )}
+          styles={buttonCustomStyles}
+        />
+        <ScrollView contentContainerStyle={styles.scrollview}>
+          {cards.map((card) => {
+            if (card.type === 'title') return <TitleCard key={card.id} {...card} />
 
-          const CardComponent = cardComponentMap[card.type]
-          return <CardComponent key={card.id} {...card} />
-        })}
+            const CardComponent = cardComponentMap[card.type]
+            return <CardComponent key={card.id} {...card} />
+          })}
+        </ScrollView>
       </Layout>
     </ActionSheetProvider>
   )
@@ -31,4 +52,25 @@ const cardComponentMap = {
   long: TextCard,
   radio: SelectCard,
   checkbox: SelectCard,
+}
+
+const styles = StyleSheet.create({
+  scrollview: {
+    paddingBottom: 30,
+    marginVertical: 20,
+    alignItems: 'center',
+  },
+})
+const buttonCustomStyles: ButtonCustomStyles = {
+  button: {
+    paddingVertical: 10,
+    marginHorizontal: 10,
+    justifyContent: 'center',
+    backgroundColor: colors.purple,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: colors.greyDark,
+  },
 }
