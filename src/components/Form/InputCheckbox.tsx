25d97f1a9f9855c125df5649abe99f133a99f5bd
jjanmo <jjanmo07@hanmail.net>
2023-08-18 02:07:42 +0900

    fix: fix multiple checkbox validation

--- src/components/Form/InputCheckbox.tsx
+++ src/components/Form/InputCheckbox.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
 import { Checkbox } from 'react-native-paper'
-import { Controller, useFormContext } from 'react-hook-form'
+import { Controller, FieldValues, useFormContext } from 'react-hook-form'
 import { SurveyCardType } from '@store/slice/cardSlice.type'
 import { colors } from '@styles/theme'
 import InputQuestion from './common/InputQuestion'
@@ -8,10 +9,19 @@ import InputWrapper from './common/InputWrapper'
 import ErrorNotice from '@components/ErrorNotice'
 
 export default function InputCheckbox({ id, question, options, required }: SurveyCardType) {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext()
+  const { control } = useFormContext()
+
+  const [isError, setIsError] = useState<boolean>(false)
+
+  const customValidate = (_: any, values: FieldValues) => {
+    if (required) {
+      const isValid = values[id].filter(Boolean).length > 0
+      setIsError(!isValid)
+      return isValid
+    }
+
+    return undefined
+  }
 
   return (
     <InputWrapper>
@@ -21,28 +31,29 @@ export default function InputCheckbox({ id, question, options, required }: Surve
         {options.map((option, index) => (
           <Controller
             key={option.id}
+            defaultValue={false}
             control={control}
             rules={{
-              required,
+              validate: customValidate,
             }}
             render={({ field: { onChange, value } }) => (
-              <>
-                <View style={styles.optionContainer}>
-                  <Checkbox.Android
-                    style={styles.checkbox}
-                    color={colors.purpleDark}
-                    status={value ? 'checked' : 'unchecked'}
-                    onPress={() => onChange(!value)}
-                  />
-                  <Text>{option.text}</Text>
-                </View>
-                {errors[id] && <ErrorNotice message={'필수 질문입니다.'} />}
-              </>
+              <View key={option.id} style={styles.optionContainer}>
+                <Checkbox.Android
+                  style={styles.checkbox}
+                  color={colors.purpleDark}
+                  status={value ? 'checked' : 'unchecked'}
+                  onPress={() => {
+                    onChange(!value)
+                  }}
+                />
+                <Text>{option.text}</Text>
+              </View>
             )}
             name={`${id}[${index}]`}
           />
         ))}
       </View>
+      {!!isError && <ErrorNotice message={'필수 질문입니다.'} />}
     </InputWrapper>
   )
 }
