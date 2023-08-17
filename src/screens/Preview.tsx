0e164aa6c98f3bb454c1e9635d0fc027668abc40
jjanmo <jjanmo07@hanmail.net>
2023-08-17 21:51:25 +0900

    feat: add submit logic

--- src/screens/Preview.tsx
+++ src/screens/Preview.tsx
@@ -1,27 +1,38 @@
 import { Alert, ScrollView, StyleSheet, View } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { FormProvider, useForm } from 'react-hook-form'
+import { useNavigation } from '@react-navigation/native'
 import { FontAwesome } from '@expo/vector-icons'
 import { RootState } from '@store/root'
 import { CardType } from '@store/slice/cardSlice.type'
+import { AnswerState, answerActions } from '@store/slice/answerSlice'
 import { InputCheckbox, InputRadio, InputTextField } from '@components/Form'
 import Layout from '@components/Layout'
 import TitleView from '@components/TitleView'
 import Button, { ButtonCustomStyles } from '@components/Button'
 import { colors } from '@styles/theme'
 
-interface FormData {
-  [key: string]: string | boolean[]
-}
-
 export default function Preview() {
   const cards = useSelector<RootState, CardType[]>((state) => state.cards.data)
-
+  const dispatch = useDispatch()
   const methods = useForm()
-  const onSubmit = (data: FormData) => {
-    console.log('🤬', data)
+  const navigation = useNavigation()
 
-    // dispatch response
+  const onSubmit = (data: AnswerState['data']) => {
+    if (cards.length <= 1) return Alert.alert('설문지를 생성 후 제출할 수 있습니다.')
+
+    Alert.alert('설문 응답을 제출합니다.', '설문을 제출하면 기존 응답은 삭제됩니다.', [
+      {
+        text: '취소',
+      },
+      {
+        text: '제출',
+        onPress: () => {
+          dispatch(answerActions.updateAnswers({ data }))
+          navigation.navigate('Result')
+        },
+      },
+    ])
   }
 
   return (
@@ -36,7 +47,7 @@ export default function Preview() {
           />
         </View>
         <ScrollView contentContainerStyle={styles.scrollview}>
-          {cards.map((card, index) => {
+          {cards.map((card) => {
             if (card.type === 'title') return <TitleView key={card.id} {...card} />
             if (card.type === 'checkbox') return <InputCheckbox key={card.id} {...card} />
             if (card.type === 'radio') return <InputRadio key={card.id} {...card} />
