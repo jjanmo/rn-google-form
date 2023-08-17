315ed64ce7b9cc51808af84ea0d58bf32ebdd241
jjanmo <jjanmo07@hanmail.net>
2023-08-17 17:46:22 +0900

    fix: fix short-long-rerendering and change input-style

--- src/components/Form/InputTextField.tsx
+++ src/components/Form/InputTextField.tsx
@@ -1,38 +1,58 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { TextInput } from '@react-native-material/core'
 import { SurveyCardType } from '@store/slice/cardSlice.type'
 import { colors } from '@styles/theme'
 import InputQuestion from './common/InputQuestion'
 import InputWrapper from './common/InputWrapper'
+import { Controller, useFormContext } from 'react-hook-form'
 
-export default function InputTextField({ question, required, type }: SurveyCardType) {
-  const isShort = useRef<boolean>(type === 'short')
-  const [value, setValue] = useState('')
+export default function InputTextField({ id, question, required, type }: SurveyCardType) {
+  const { control } = useFormContext()
+  const [isShort, setIsShort] = useState<boolean>(false)
 
-  const handleChangeAnswer = (text: string) => {
-    setValue(text)
-  }
+  useEffect(() => {
+    setIsShort(type === 'short' ? true : false)
+  }, [type])
 
   return (
     <InputWrapper>
       <InputQuestion question={question} required={required} />
-
-      <TextInput
-        value={value}
-        onChangeText={handleChangeAnswer}
-        placeholder="내 답변"
-        variant="standard"
-        inputContainerStyle={isShort.current ? styles.half : null}
-        selectionColor="grey"
-        color={colors.purpleDark}
+      <Controller
+        control={control}
+        rules={{
+          required,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            value={value}
+            onChangeText={onChange}
+            placeholder="내 답변"
+            variant={isShort ? 'standard' : 'outlined'}
+            inputContainerStyle={isShort ? styles.shortInputContainer : null}
+            inputStyle={isShort ? null : styles.longInput}
+            selectionColor="grey"
+            color={colors.purpleDark}
+            maxLength={isShort ? 10 : 500}
+            multiline={!isShort}
+          />
+        )}
+        name={id}
       />
     </InputWrapper>
   )
 }
 
 const styles = StyleSheet.create({
-  half: {
+  shortInputContainer: {
     width: '50%',
+  },
+  longInput: {
+    height: 100,
+    borderColor: colors.grey,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
   },
 })
